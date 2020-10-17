c6f75964e334c562316a5048c3c0c9e161c3b8ef
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-10-17 14:56:00 +0200

    start vtc jobs

---
+++ run_vtc.py
@@ -0,0 +1,23 @@
+import subprocess
+import argparse
+import os
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--inputs", nargs="+", default=[])
+parser.add_argument("--destinations", nargs="+", default=[])
+args = parser.parse_args()
+
+inputs = list(map(os.path.abspath, args.inputs))
+destinations = list(map(os.path.abspath, args.destinations))
+
+if len(inputs) != len(destinations):
+    print('inputs length does not match destinations length')
+    sys.exit(1)
+
+os.chdir('voice_type_classifier')
+
+for input in inputs:
+    subprocess.call(['./apply.sh', input, '--device=gpu'])
+
+print('complete')
---
+++ start.py
@@ -0,0 +1,66 @@
+from ChildProject.projects import ChildProject
+import argparse
+import sys
+import pandas as pd
+import subprocess
+import os
+import wave
+import time
+import datetime
+from functools import reduce
+import operator
+
+subprocess.call(['git', 'clone', '--recurse-submodules', 'https://github.com/MarvinLvn/voice_type_classifier.git'])
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument("--source", help = "path to project", required = True)
+args = parser.parse_args()
+
+project = ChildProject(args.source)
+errors, warnings = project.validate_input_data()
+
+if len(errors) > 0:
+    print("validation failed, {} error(s) occured".format(len(errors)), file = sys.stderr)
+    print("cannot import data", file = sys.stderr)
+    sys.exit(1)
+
+def get_audio_duration(filename):
+    f = wave.open(filename,'r')
+    return f.getnframes() / float(f.getframerate())
+
+recordings = project.recordings
+recordings['duration'] = project.recordings['filename'].map(lambda f:
+    get_audio_duration(os.path.join(project.path, 'recordings', f))
+)
+# GPU computation time upper bound according to https://docs.google.com/presentation/d/1JTM_e56RSCpHqzq0VDu8Qude7P5UNKM6v18LT4jI7Do/edit#slide=id.ga0712b0b07_0_16
+recordings['vtc_computation_time_estimate'] = recordings['duration'] * 0.57/20 * 2
+
+# do the splitting by child_id for now
+for group, group_recordings in recordings.groupby('child_id'):
+    inputs = group_recordings['filename'].map(lambda f: os.path.join(project.path, 'recordings', f)).tolist()
+    destinations = group_recordings['filename'].map(lambda f: os.path.join(project.path, 'raw_annotations/vtc', f + '.rttm')).tolist()
+
+    computation_time = group_recordings['vtc_computation_time_estimate'].sum()
+    job_name = 'vtc_{}_{}'.format(group, datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+
+    proc = subprocess.Popen(
+        [
+            'sbatch',
+            '--job-name=' + job_name,
+            '--mem=30G',
+            '--time=' + time.strftime("%H:%M:%S", time.gmtime(computation_time)),
+            '--output=' + os.path.join(project.path, 'raw_annotations/vtc', job_name + '.out'), 
+            '--error=' + os.path.join(project.path, 'raw_annotations/vtc', job_name + '.err'), 
+
+            'python', 'run_vtc.py', '--inputs'
+        ]
+        + inputs + 
+        [
+            '--destinations'
+        ]
+        + destinations
+    )
+
+
+# proc = subprocess.Popen()
+
