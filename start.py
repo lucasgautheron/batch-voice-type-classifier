c4670103e35c02a2944ee9102a4d96257b51e9fd
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-07-14 15:38:15 +0200

    custom recordings list

--- start.py
+++ start.py
@@ -14,6 +14,7 @@ parser.add_argument("--profile", help = "audio profile to be used", default = ""
 parser.add_argument("--mem", help = "slurm jobs memory in GB", default = 30, type = int)
 parser.add_argument("--batch", default = 8, type = int)
 parser.add_argument("--overwrite", help = "overwrite rttm if exists", default = False, required = False)
+parser.add_argument("--recordings", help = "recordings whitelist", default = [], nargs = '+')
 args = parser.parse_args()
 
 project = ChildProject(args.source)
@@ -25,9 +26,13 @@ if len(errors) > 0:
 
 audio_prefix = os.path.join('recordings/converted', args.profile) if args.profile else 'recordings/raw'
 
+recordings = project.recordings
+
+if args.recordings:
+    recordings = recordings[recordings['recording_filename'].isin(args.recordings)]
+
 if args.profile:
     converted = pd.read_csv(os.path.join(project.path, audio_prefix, 'recordings.csv'))
-    recordings = project.recordings
     recordings = recordings.merge(converted, left_on = 'recording_filename', right_on = 'original_filename')
     recordings['input'] = recordings['converted_filename'].map(lambda f: os.path.join(project.path, audio_prefix, f))
 else:
@@ -56,7 +61,7 @@ recordings.reset_index(inplace = True)
 
 # GPU computation time upper bound according to https://docs.google.com/presentation/d/1JTM_e56RSCpHqzq0VDu8Qude7P5UNKM6v18LT4jI7Do/edit#slide=id.ga0712b0b07_0_16
 recordings['vtc_computation_time_estimate'] = recordings['duration'] * 0.57/20 * 6
-target_computation_time = 20*3600
+target_computation_time = 2*3600
 batches = recordings['vtc_computation_time_estimate'].sum()/target_computation_time
 recordings['batch'] = (batches*recordings.index/recordings.shape[0]).astype(int)
 
@@ -77,9 +82,7 @@ for group, group_recordings in recordings.groupby('batch'):
         '--mem={}G'.format(args.mem),
         '--time=' + time.strftime("%H:%M:%S", time.gmtime(computation_time)),
         '--output=' + os.path.join(project.path, 'annotations/vtc/raw', job_name + '.out'), 
-        '--error=' + os.path.join(project.path, 'annotations/vtc/raw', job_name + '.err'), 
-        '--exclude=puck5',
-
+        '--error=' + os.path.join(project.path, 'annotations/vtc/raw', job_name + '.err'),
         './run_vtc.py', '--inputs'
     ] + group_recordings['input'].tolist() + ['--destinations'] + group_recordings['destination'].tolist() + ['--tmpnames'] + group_recordings['tmpname'].tolist() + ['--batch', str(args.batch)]
 
